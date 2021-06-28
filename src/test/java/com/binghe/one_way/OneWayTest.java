5e5791d60bc049f1c38ec75af9eb89e50ab70119
binghe819 <binghe819@naver.com>
2021-06-29 00:08:11 +0900

    test: 연관관계 주인 단방향 테스트 추가

---
+++ src/test/java/com/binghe/one_way/OneWayTest.java
@@ -0,0 +1,59 @@
+package com.binghe.one_way;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.binghe.one_way.Member;
+import com.binghe.one_way.Team;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("연관관계 주인 단방향 테스트")
+public class OneWayTest {
+
+    @DisplayName("")
+    @Test
+    void owner() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("test_persistence_config");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction tx = entityManager.getTransaction();
+
+        tx.begin();
+
+        try {
+            // given
+            Team team = new Team();
+            team.setName("Team A");
+            entityManager.persist(team);
+
+            Member member = new Member();
+            member.setName("binghe");
+            member.setTeam(team);
+            entityManager.persist(member);
+
+            entityManager.flush();
+            entityManager.clear();
+
+            // when
+            Member findMember = entityManager.find(Member.class, member.getId());
+            Team findTeam = findMember.getTeam();
+
+            // then
+            assertThat(findMember.getName()).isEqualTo("binghe");
+            assertThat(findTeam.getName()).isEqualTo("Team A");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            entityManager.close();
+        }
+        entityManagerFactory.close();
+    }
+}
