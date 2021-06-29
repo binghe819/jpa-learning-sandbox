9928f76a5535c717b2c4603170a11df2aa8c7f04
binghe819 <binghe819@naver.com>
2021-06-29 16:20:22 +0900

    test: N : N 단방향 테스트 추가

---
+++ src/test/java/com/binghe/one_way/OneWayTest.java
@@ -0,0 +1,35 @@
+package com.binghe.one_way;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("N : N 단방향 테스트")
+public class OneWayTest {
+
+    @DisplayName("DB 테이블 생성 테스트용 - N : N은 연결 테이블을 만든다.")
+    @Test
+    void oneWay() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("test_persistence_config");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        try {
+
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println("Error!!! " + e.getMessage());
+            tx.rollback();
+        } finally {
+            entityManager.close();
+        }
+        entityManagerFactory.close();
+    }
+
+}
