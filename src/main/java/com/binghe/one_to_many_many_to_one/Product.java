71aee5c73fa30b36195d27c77e83b425df1a4bec
binghe819 <binghe819@naver.com>
2021-06-29 16:53:40 +0900

    test: N : N 한계를 극복하기 위한 1 : N + N : 1 테스트 추가

---
+++ src/main/java/com/binghe/one_to_many_many_to_one/Product.java
@@ -0,0 +1,48 @@
+package com.binghe.one_to_many_many_to_one;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Product {
+
+    @Id @GeneratedValue
+    @Column(name = "PRODUCT_ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @OneToMany(mappedBy = "product")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<MemberProduct> getMemberProducts() {
+        return memberProducts;
+    }
+
+    public void setMemberProducts(
+        List<MemberProduct> memberProducts) {
+        this.memberProducts = memberProducts;
+    }
+}
