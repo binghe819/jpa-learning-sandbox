c5b497d00422c7ae2fb3af576a316cb1b4f05910
binghe819 <binghe819@naver.com>
2021-07-01 22:11:12 +0900

    feat: 영속성 전이(cascade) 테스트 추가

--- src/main/java/com/binghe/Parent.java
+++ src/main/java/com/binghe/Parent.java
@@ -2,6 +2,7 @@ package com.binghe;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -15,7 +16,7 @@ public class Parent {
 
     private String name;
 
-    @OneToMany(mappedBy = "parent")
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
     private List<Child> childs = new ArrayList<>();
 
     public void addChild(Child child) {
@@ -37,5 +38,9 @@ public class Parent {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Child> getChilds() {
+        return childs;
     }
 }
---
+++ src/test/java/com/binghe/CascadeTest.java
@@ -0,0 +1,55 @@
+package com.binghe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("영속성 전이 (CASCADE) 테스트")
+public class CascadeTest {
+
+    @Test
+    void cascadeTest() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("test_persistence_config");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        try {
+            Child child1 = new Child();
+            child1.setName("child1");
+            Child child2 = new Child();
+            child2.setName("child2");
+
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            // Parent만 영속화하면 안에 들어있는 child도 자동으로 영속화된다.
+            entityManager.persist(parent);
+
+            entityManager.flush();
+            entityManager.clear();
+
+            Parent findParent = entityManager.find(Parent.class, parent.getId());
+
+            assertThat(findParent.getChilds())
+                .extracting("name")
+                .containsExactly("child1", "child2");
+
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println("Error!! -> " + e);
+            tx.rollback();
+        } finally {
+            entityManager.close();
+        }
+        entityManagerFactory.close();
+    }
+}
