b91b8ae79f31e9820fb463b531cb0257412d7183
binghe819 <binghe819@naver.com>
2021-07-01 21:36:21 +0900

    feat: Parent, Child 엔티티 정의

---
+++ src/main/java/com/binghe/Parent.java
@@ -0,0 +1,41 @@
+package com.binghe;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Parent {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Child> childs = new ArrayList<>();
+
+    public void addChild(Child child) {
+        this.childs.add(child);
+        child.setParent(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
