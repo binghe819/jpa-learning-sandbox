dd53666dcdd0011cd2b4af7680b8ef9f24427abf
binghe819 <binghe819@naver.com>
2021-07-03 21:03:05 +0900

    feat: StationRepository 구현 (테스트 포함)

--- src/main/java/com/binghe/subway/domain/Station.java
+++ src/main/java/com/binghe/subway/domain/Station.java
@@ -54,4 +54,9 @@ public class Station {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    public void changeName(String name) {
+        this.name = name;
+
+    }
 }
--- src/test/java/com/binghe/subway/repository/StationRepositoryTest.java
+++ src/test/java/com/binghe/subway/repository/StationRepositoryTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.binghe.subway.domain.Station;
-import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,14 +31,36 @@ class StationRepositoryTest {
         assertThat(actual.getName()).isEqualTo("잠실역");
     }
 
-    @DisplayName("영속성 컨텍스트를 이용해서 동등성과 동일성을 지원한다.")
+    @DisplayName("영속성 컨텍스트를 이용하면 1차 캐싱을 이용하여 동일성을 지원한다.")
     @Test
-    void equals() {
+    void equalsAndSame() {
         Station station1 = stations.save(new Station("잠실역"));
         Station station2 = stations.findByName("잠실역");
         assertThat(station1.getId()).isNotNull();
         assertThat(station2.getId()).isNotNull();
         assertThat(station1).isEqualTo(station2);
         assertThat(station1).isSameAs(station2);
+    }
+
+    @DisplayName("영속성 컨텍스트의 더티체킹을 이용하여 변경을 감지하여 자동으로 UPDATE 쿼리를 날린다. -> 테스트 필요")
+    @Test
+    void update() {
+        Station station = stations.save(new Station("잠실역"));
+        station.changeName("몽촌토성역");
+
+        Station actual = stations.findByName("몽촌토성역");
+        assertThat(actual).isNotNull();
+        assertThat(actual.getName()).isEqualTo("몽촌토성역");
+    }
+
+    @DisplayName("1차 캐싱을 이용한 테스트 -> 토론 필요 (로깅에 나오는 것이 정말 쿼리를 날린다는 것인가?)")
+    @Test
+    void cache() {
+        Station station = stations.save(new Station("잠실역"));
+        Station findStation1 = stations.findByName("잠실역");
+        Station findStation2 = stations.findByName("잠실역");
+
+        assertThat(station).isSameAs(findStation1);
+        assertThat(findStation1).isSameAs(findStation2);
     }
 }
