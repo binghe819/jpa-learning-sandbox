453b1289f0869477c0d6187b8cb56c291e9c8a91
binghe819 <binghe819@naver.com>
2021-07-14 02:52:15 +0900

    test: 집합과 정렬 테스트만 정의

---
+++ src/test/java/com/binghe/SetAndSortTest.java
@@ -0,0 +1,33 @@
+package com.binghe;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+// 1
+@DisplayName("집합과 정렬 테스트")
+public class SetAndSortTest {
+
+    @DisplayName("")
+    @Test
+    void sum_avg_max_min() {
+
+    }
+
+    @DisplayName("GROUP BY")
+    @Test
+    void groupBy() {
+
+    }
+
+    @DisplayName("GROUP BY HAVING")
+    @Test
+    void groupByHaving() {
+
+    }
+
+    @DisplayName("ORDER BY")
+    @Test
+    void orderBy() {
+
+    }
+}
