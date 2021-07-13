ba84b4caed9ca10dd84860a6ae4f2a04f8d6c545
binghe819 <binghe819@naver.com>
2021-07-14 02:55:24 +0900

    chore: 정의하지 못한 부분 정의

--- src/main/java/com/binghe/Product.java
+++ src/main/java/com/binghe/Product.java
@@ -17,4 +17,35 @@ public class Product {
 
     private int stockAmount;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStockAmount() {
+        return stockAmount;
+    }
+
+    public void setStockAmount(int stockAmount) {
+        this.stockAmount = stockAmount;
+    }
 }
