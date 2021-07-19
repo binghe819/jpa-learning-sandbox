1e0fbe7b188e3745b662c5794a4b805bf2cb89fd
binghe819 <binghe819@naver.com>
2021-07-20 02:56:14 +0900

    test: Join 테스트 추가

--- src/main/java/com/binghe/Team.java
+++ src/main/java/com/binghe/Team.java
@@ -2,6 +2,7 @@ package com.binghe;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,8 +17,13 @@ public class Team {
 
     private String name;
 
-    @OneToMany(mappedBy = "team")
+    @OneToMany(mappedBy = "team", cascade = CascadeType.PERSIST)
     private List<Member> members = new ArrayList<>();
+
+    public void addMember(Member member) {
+        member.setTeam(this);
+        this.members.add(member);
+    }
 
     public Long getId() {
         return id;
