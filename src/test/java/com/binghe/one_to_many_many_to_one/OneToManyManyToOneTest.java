609b756a8063a9ce07a8ad9c9adf532c71f81b1b
binghe819 <binghe819@gmail.com>
2021-10-25 12:45:56 +0900

    refactor: EntityManagerTemplate으로 리팩토링

--- src/test/java/com/binghe/one_to_many_many_to_one/OneToManyManyToOneTest.java
+++ src/test/java/com/binghe/one_to_many_many_to_one/OneToManyManyToOneTest.java
@@ -1,34 +1,30 @@
 package com.binghe.one_to_many_many_to_one;
 
+import com.binghe.template.EntityManagerTemplate;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("N : N의 한계 극복 테스트")
 public class OneToManyManyToOneTest {
 
+    private EntityManagerTemplate entityManagerTemplate;
+
+    @BeforeEach
+    void setUp() {
+        entityManagerTemplate = new EntityManagerTemplate();
+    }
+
     @DisplayName("N : N의 한계를 극복하기 위해 1 : N + N : 1로 구성하는 예시")
     @Test
     void test() {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("test_persistence_config");
-
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-
-        EntityTransaction tx = entityManager.getTransaction();
-        tx.begin();
-
-        try {
-
+        entityManagerTemplate.execute(((entityManager, tx) -> {
             tx.commit();
-        } catch (Exception e) {
-            System.out.println("Error!!! " + e.getMessage());
-            tx.rollback();
-        } finally {
-            entityManager.close();
-        }
-        entityManagerFactory.close();
+        }));
     }
 }
--- src/test/java/com/binghe/one_way/OneWayTest.java
+++ src/test/java/com/binghe/one_way/OneWayTest.java
@@ -1,35 +1,30 @@
 package com.binghe.one_way;
 
+import com.binghe.template.EntityManagerTemplate;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("N : N 단방향 테스트")
 public class OneWayTest {
 
+    private EntityManagerTemplate entityManagerTemplate;
+
+    @BeforeEach
+    void setUp() {
+        entityManagerTemplate = new EntityManagerTemplate();
+    }
+
     @DisplayName("DB 테이블 생성 테스트용 - N : N은 연결 테이블을 만든다.")
     @Test
     void oneWay() {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("test_persistence_config");
-
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-
-        EntityTransaction tx = entityManager.getTransaction();
-        tx.begin();
-
-        try {
-
+        entityManagerTemplate.execute(((entityManager, tx) -> {
             tx.commit();
-        } catch (Exception e) {
-            System.out.println("Error!!! " + e.getMessage());
-            tx.rollback();
-        } finally {
-            entityManager.close();
-        }
-        entityManagerFactory.close();
+        }));
     }
 
 }
