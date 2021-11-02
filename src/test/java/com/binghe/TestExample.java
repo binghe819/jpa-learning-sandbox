8f12d00111176ad47819555a199f48fa9e0295cb
binghe819 <binghe819@gmail.com>
2021-11-02 23:04:35 +0900

    refactor: EntityManagerTemplate으로 리팩토링

--- src/test/java/com/binghe/TestExample.java
+++ src/test/java/com/binghe/TestExample.java
@@ -1,33 +1,24 @@
 package com.binghe;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import com.binghe.template.EntityManagerTemplate;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TestExample {
 
+    private EntityManagerTemplate entityManagerTemplate;
+
+    @BeforeEach
+    void setUp() {
+        entityManagerTemplate = new EntityManagerTemplate();
+    }
+
     @DisplayName("테스트 예시")
     @Test
     void createQuery_Empty_ReturnEmptyList() {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("test_persistence_config");
-
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-
-        EntityTransaction tx = entityManager.getTransaction();
-        tx.begin();
-
-        try {
-
+        entityManagerTemplate.execute(((entityManager, tx) -> {
             tx.commit();
-        } catch (Exception e) {
-            System.out.println("Error!! -> " + e);
-            tx.rollback();
-        } finally {
-            entityManager.close();
-        }
-        entityManagerFactory.close();
+        }));
     }
 }
